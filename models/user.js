ddf8853aeb128c8fc3bf38e9d07c6dfb81eb6bc7
unknown <shalinisai9195@gmail.com>
2023-06-26 14:58:25 +0530

    updatecommit

--- models/user.js
+++ models/user.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
 })
 
 userSchema.methods.generateAuthToken = function(){
-   const token = jwt.sign({_id:this.id},process.env.JWTPRIVATEKEY,{expiresIn:'1hr'});
+   const token = jwt.sign({_id:this.id},process.env.JWTPRIVATEKEY,{expiresIn:'7d'});
    return token;
 }
 
